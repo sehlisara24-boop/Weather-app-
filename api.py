de9190b851d9b8dff9d332b7dcb1dd23db4bb9fe
Sara Sehli <sehlisara24@gmail.com>
2026-04-14 13:52:43 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,15 +1,1 @@
-import requests
 
-def get_weather(city):
-    api_key = "YOUR_API_KEY"
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-
-    response = requests.get(url)
-    data = response.json()
-
-    weather = {
-        "temp": data["main"]["temp"],
-        "humidity": data["main"]["humidity"],
-        "status": data["weather"][0]["description"]
-    }
-    return weather
